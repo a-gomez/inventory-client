50d3525027fb8fc148b7854d950f42d303cef36d
Ann Gomez <annbgomez@gmail.com>
2012-02-09 10:58:31 +0100

    formatting README

--- features/step_definitions/info_steps.rb
+++ features/step_definitions/info_steps.rb
@@ -13,7 +13,7 @@ Then /^the computer specs in hash format should be (.*)$/ do |internal_hash|
   @output.to_s.should == internal_hash.to_s
 end
 
-Given /^we run the inventory script with the computer specs in JSON format "([^"]*)"$/ do |input_JSON|
+Given /^we run the inventory script with the specs in JSON format "([^"]*)"$/ do |input_JSON|
   @output = `ruby inventory.rb #{input_JSON}`
   raise('Command failed!') unless $?.success?
 end
